7d9dc9f5b2638208f3b463348440943a0e632171
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-06-25 11:10:06 +0900

    ♻️refactor: file colocations

---
+++ app/area/layout.tsx
@@ -0,0 +1,13 @@
+import type React from "react"
+
+export default function AreaLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <article className="grid gap-6 p-4 text-center text-xs sm:px-12">
+      {children}
+    </article>
+  )
+}
