11e6814a65a1d8fb25f2509c2f9cbfb95d81e21c
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-07-31 10:53:10 +0900

    ✨feat: add details dom to faq

--- app/history/hiroshima/faq/page.tsx
+++ app/history/hiroshima/faq/page.tsx
@@ -2,6 +2,6 @@ import { QandAs } from "@/app/components/layout/qAndA"
 import { HIROSHIMA, HIROSHIMA_LINE, HISTORY } from "@/app/lib/constant"
 import type { JSX } from "react"
 
-export default function QandA(): JSX.Element {
+export default function FAQ(): JSX.Element {
   return <QandAs lineLink={HIROSHIMA_LINE} menu={HISTORY} submenu={HIROSHIMA} />
 }
